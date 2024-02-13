4853e529eb5f8bb0aa3eeb423797276d459e73fa
sahir <sahir@pop-os.localdomain>
2024-02-13 16:37:45 +0530

    feat: setting up repositories container

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RepocardComponent } from '../repocard/repocard.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 @Component({
   selector: 'app-repositories',
   standalone: true,
-  imports: [],
+  imports: [RepocardComponent,PaginationComponent],
   templateUrl: './repositories.component.html',
   styleUrl: './repositories.component.css'
 })
