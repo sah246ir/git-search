595372063a3ee78f0f40d628a6bc2319de7eb733
sahir <sahir@pop-os.localdomain>
2024-02-13 18:00:17 +0530

    feat: adding routing for search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+export const routes: Routes = [
+    {
+        component:HomeComponent,
+        path:''
+    },
+    {
+        component:RepositoriesComponent,
+        path:'search'
+    }
+];
