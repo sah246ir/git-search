e37806ed75e7a1a09b495daa4a59a8546e463679
sahir <sahir@pop-os.localdomain>
2024-02-13 17:35:02 +0530

    feat: adding functions for fetching sample data

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { PaginationComponent } from '../pagination/pagination.component'; 
 import { RepocardComponent } from '../repocard/repocard.component';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-repositories',
   standalone: true,
-  imports: [PaginationComponent,RepocardComponent],
+  imports: [PaginationComponent,RepocardComponent,HttpClientModule],
   templateUrl: './repositories.component.html',
   styleUrl: './repositories.component.css'
 })
-export class RepositoriesComponent {
+export class RepositoriesComponent implements OnInit {
+  http = inject(HttpClient)
+  data:any[] = []
+
+  ngOnInit(): void {
+    this.fetchdata();
+   }
+
+  fetchdata(){
+    this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe((data:any)=>{ 
+      this.data = data.slice(1,5)
+    })
+  }
 
 }
