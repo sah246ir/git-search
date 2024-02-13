7a99bd99aad71fdc21c719486b31fc72197d37d8
sahir <sahir@pop-os.localdomain>
2024-02-13 16:29:44 +0530

    init: setting up components for app

---
+++ src/app/components/repocard/repocard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repocard',
+  standalone: true,
+  imports: [],
+  templateUrl: './repocard.component.html',
+  styleUrl: './repocard.component.css'
+})
+export class RepocardComponent {
+
+}
